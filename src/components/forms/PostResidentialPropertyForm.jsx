6c9fe3579178ec78b37ffb81ffcecd631127e437
BishalSunuwar202 <bishalsunuwar24@gmail.com>
2025-08-28 12:40:59 +0545

    added controller for amenities from server

--- src/components/forms/PostResidentialPropertyForm.jsx
+++ src/components/forms/PostResidentialPropertyForm.jsx
@@ -1,54 +1,91 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
+import usePostResidential from "../../hooks/usePostResidential";
+import { useAmenities } from "../../hooks/useAmenities";
 
 const PostResidentialPropertyForm = () => {
   const {
     register,
     handleSubmit,
+    control,
     watch,
-    // formState: { errors },
+    setError,
+    formState: { errors },
   } = useForm({
     defaultValues: {
       title: "",
-      price: "",
-      propertyType: ["Buy", "Rent"],
-      bannerImage: "",
+      price: null,
+      //propertyType: ["Buy", "Rent"],
+      buy: false,
+      rent: false,
+      banner_photo: null,
       address: {
         street1: "",
         street2: "",
-        municipality: "",
-        ward: "",
-        district: "",
         province: "",
+        district: "",
+        municipality: "",
+        ward: null,
       },
-      amenities: ["bed", "light", "water"],
-      propertyPlan: "",
-      mapEmbeded: "",
-      storey: "",
-      totalHouseArea: "",
-      totalLandArea: "",
+      //amenities: ["bed", "light", "water"],
+      amenities: [],
+      property_plan_id: null,
+      storey: null,
+      total_house_area: "",
+      total_land_area: "",
       face: "",
-      faceSize: "",
-      roadInfrontOfHouse: "",
-      youtubeLink: "",
-      nearLandMark: "",
-      propertyDetails: "",
-      otherDetails: "",
-      noteDetails: "",
-      otherImages: "",
+      face_size: "",
+      road_in_front_of_house: "",
+      near_landmark: "",
+      property_details: "",
+      other_details: "",
+      note_details: "",
+      other_photos: [],
+      map_embed: "",
+      youtube_link: "",
     },
   });
-  const municipality = watch("address.municipality");
-  return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
-      {/* <div>
-        <input placeholder="" {...register("lastName", { required: true })} /> 
-        {errors.lastName && <p>Last name is required.</p>}
-      </div> */}
 
-      {/* <input placeholder="" {...register("age", { pattern: /\d+/ })} /> */}
-      {/* {errors.age && <p>Please enter number for age.</p>} */}
-      {/* Title */}
+  const mutation = usePostResidential();
+  console.log("eroors in form", errors);
+  const onSubmit = (data) => {
+    console.log(data);
+    mutation.mutate(data, {
+      onSuccess: () => {
+        console.log("signup successful");
+        //queryClient.invalidateQueries(["users"]); // if you want to refetch users list
+      },
+      onError: (err) => {
+        // console.log(err || "post failed");
+
+        if (!err) return;
+
+        Object.keys(err).forEach((field) => {
+          if (typeof err[field] === "object" && !Array.isArray(err[field])) {
+            // nested object, like "address"
+            Object.keys(err[field]).forEach((nestedField) => {
+              const formField = `${field}.${nestedField}`; // e.g., "address.street1"
+              setError(formField, {
+                type: "server",
+                message: err[field][nestedField][0],
+              });
+            });
+          } else {
+            // regular field
+            setError(field, { type: "server", message: err[field][0] });
+          }
+        });
+      },
+    });
+  };
+  const municipality = watch("address.municipality");
+
+  //for aminities data
+  const { data: dataAmenities } = useAmenities();
+  console.log(dataAmenities);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label>Title</label>
         <input placeholder="Property Title" {...register("title")} />
@@ -56,6 +93,7 @@ const PostResidentialPropertyForm = () => {
 
       {/* Price */}
       <div>
+        <div style={{ color: "red" }}>{errors.title?.message}</div>
         <label>Price</label>
         <input placeholder="Price" type="number" {...register("price")} />
       </div>
@@ -64,11 +102,11 @@ const PostResidentialPropertyForm = () => {
       <div>
         <label>Property Type</label>
         <label>
-          <input type="checkbox" value="Buy" {...register("propertyType")} />
+          <input type="checkbox" value="Buy" {...register("buy")} />
           Buy
         </label>
         <label>
-          <input type="checkbox" value="Rent" {...register("propertyType")} />
+          <input type="checkbox" value="Rent" {...register("rent")} />
           Rent
         </label>
       </div>
@@ -76,7 +114,7 @@ const PostResidentialPropertyForm = () => {
       {/* Banner Image */}
       <div>
         <label>Banner Image</label>
-        <input type="file" {...register("bannerImage")} />
+        <input type="file" {...register("banner_photo")} />
       </div>
 
       {/* Address (Nested) */}
@@ -113,21 +151,62 @@ const PostResidentialPropertyForm = () => {
       </fieldset>
 
       {/* Amenities */}
-      <div>
+      <div htmlFor="Amenities">Amenitites</div>
+      <Controller
+        control={control}
+        name="amenities"
+        render={({ field }) => (
+          <div>
+            {dataAmenities?.map((amenity) => (
+              <label key={amenity.id}>
+                {amenity.name}
+                <input
+                  type="checkbox"
+                  // Check if the current value is included in the array of selected items
+                  checked={field.value?.some(
+                    // this checked is attribute of input
+                    (item) => item.id === amenity.id
+                  )}
+                  onChange={(e) => {
+                    const checked = e.target.checked;
+                    const newArray = checked
+                      ? [...(field.value ?? []), amenity]
+                      : field.value?.filter((item) => item.id !== amenity.id);
+                    field.onChange(newArray);
+                  }}
+                />
+              </label>
+            ))}
+          </div>
+        )}
+      />
+      {/* <div>
         <label>Amenities</label>
         <label>
-          <input type="checkbox" value="bed" {...register("amenities")} />
+          <input
+            type="checkbox"
+            id="1"
+            value="bed"
+            {...register("Bed")}
+            onChange={(e) => {
+              const checked = e.target.checked;
+              if(checked) {
+
+                const newArray = [...dataAmenities]
+              }
+            }}
+          />
           Bed
         </label>
         <label>
-          <input type="checkbox" value="light" {...register("amenities")} />
+          <input type="checkbox" value="light" {...register("Light")} />
           Light
         </label>
         <label>
-          <input type="checkbox" value="water" {...register("amenities")} />
+          <input type="checkbox" value="water" {...register("Water")} />
           Water
         </label>
-      </div>
+      </div> */}
 
       {/* Property Plan */}
       <div>
@@ -138,7 +217,7 @@ const PostResidentialPropertyForm = () => {
       {/* Map Embed */}
       <div>
         <label>Map Embed</label>
-        <input placeholder="Map Embed Link" {...register("mapEmbeded")} />
+        <input placeholder="Map Embed Link" {...register("map_embed")} />
       </div>
 
       {/* Storey */}
@@ -160,12 +239,11 @@ const PostResidentialPropertyForm = () => {
       {/* Face */}
       <div>
         <label>Face</label>
-        <label>Face</label>
         <input placeholder="" {...register("face")} />
       </div>
       <div>
         <label>Face Size</label>
-        <input placeholder="Face Size" {...register("faceSize")} />
+        <input placeholder="Face Size" {...register("face_size")} />
       </div>
 
       {/* Road */}
@@ -173,42 +251,47 @@ const PostResidentialPropertyForm = () => {
         <label>Road In Front Of House</label>
         <input
           placeholder="Road In Front Of House"
-          {...register("roadInfrontOfHouse")}
+          {...register("road_in_front_of_house")}
         />
       </div>
 
       {/* YouTube Link */}
       <div>
         <label>YouTube Link</label>
-        <input placeholder="Youtube Link" {...register("youtubeLink")} />
+        <input placeholder="Youtube Link" {...register("youtube_link")} />
       </div>
 
       {/* Nearby Landmark */}
       <div>
         <label>Nearby Landmark</label>
-        <input placeholder="Nearby Landmark" {...register("nearLandMark")} />
+        <input placeholder="Nearby Landmark" {...register("near_landmark")} />
       </div>
 
       {/* Details */}
       <div>
         <label>Property Details</label>
-        <textarea {...register("propertyDetails")} />
+        <textarea {...register("property_details")} />
       </div>
       <div>
         <label>Other Details</label>
-        <textarea {...register("otherDetails")} />
+        <textarea {...register("other_details")} />
       </div>
       <div>
         <label>Note Details</label>
-        <textarea {...register("noteDetails")} />
+        <textarea {...register("note_details")} />
       </div>
       {/* Banner Image */}
       <div>
         <label> Other Images</label>
-        <input type="file" {...register("otherImages")} />
+        <input type="file" {...register("other_photos")} />
       </div>
 
-      <button type="submit">Post Property</button>
+      <button type="submit" disabled={mutation.isLoading}>
+        {mutation.isLoading ? "Posting..." : "Post Property"}
+      </button>
+      {mutation.isError && (
+        <p className="">{mutation.error?.response?.data?.message}</p>
+      )}
     </form>
   );
 };
